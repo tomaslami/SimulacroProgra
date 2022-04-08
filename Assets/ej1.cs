6afa726883f79f5cff8f5c7c7fdccec046166ced
LAMI GURALNIK TOM�S EZEQUIEL <46956888@ort>
2022-04-08 08:45:43 -0300

    Ej1

---
+++ Assets/ej1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        } else if (num1 != num2)
+        {
+            Debug.Log("Los numeros son diferentes");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
